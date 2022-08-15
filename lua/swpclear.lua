3ca0e283cdde3b3946dee49023edb4dce4f10717
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-16 03:14:58 +0900

    indent

--- lua/swpclear.lua
+++ lua/swpclear.lua
@@ -3,21 +3,21 @@ local module = require("swpclear.module")
 
 local M = {}
 M.config = {
-    -- default config
-    opt = "Hello!",
+  -- default config
+  opt = "Hello!",
 }
 
 -- setup is the public method to setup your plugin
 M.setup = function(args)
-    -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
-    -- you can also put some validation here for those.
-    M.config = vim.tbl_deep_extend("force", M.config, args or {})
+  -- you can define your setup function here. Usually configurations can be merged, accepting outside params and
+  -- you can also put some validation here for those.
+  M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
 
 -- "hello" is a public method for the plugin
 M.swpclr = function()
-    module.my_first_function()
+  module.my_first_function()
 end
 
 return M
--- lua/swpclear/module.lua
+++ lua/swpclear/module.lua
@@ -6,25 +6,25 @@ local buf, win
 
 M.my_first_function = function()
 
-    print("nasi")
-    buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
-    local git_log_output = api.nvim_call_function("system", { 'git log -n 30 --oneline' })
-    local git_logs = vim.split(git_log_output, "\n")
-    api.nvim_buf_set_lines(buf, 0, -1, true, git_logs)
-    api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+print("nasi")
+  buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
+  local git_log_output = api.nvim_call_function("system", { 'git log -n 30 --oneline' })
+  local git_logs = vim.split(git_log_output, "\n")
+  api.nvim_buf_set_lines(buf, 0, -1, true, git_logs)
+  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
 
 
-    local opts = {
-        style = "minimal",
-        relative = "editor",
-        width = 100,
-        height = 100,
-        row = 0,
-        col = 0
-    }
-    win = api.nvim_open_win(buf, true, opts)
-    --
-    return "ahello world!"
+  local opts = {
+    style = "minimal",
+    relative = "editor",
+    width = 100,
+    height = 100,
+    row = 0,
+    col = 0
+  }
+  win = api.nvim_open_win(buf, true, opts)
+  --
+  return "ahello world!"
 end
 
 return M
