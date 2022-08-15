b642bf0df6e4709431951c728fba84c4924e24e2
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-16 01:51:29 +0900

    ok debug

--- plugin/swpclear.lua
+++ plugin/swpclear.lua
@@ -1,2 +1,4 @@
-vim.api.nvim_create_user_command("SwpClear", require("swpclear").swpclr, {})
+package.loaded["swpclear"] = nil
+package.loaded["swpclear.module"] = nil
+vim.api.nvim_create_user_command("SwpClear", require("swpclear").swpclr, {force=true})
 
