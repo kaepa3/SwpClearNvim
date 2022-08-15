8df23b082ff305773abc7c2818d000ca0178108d
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-16 04:26:03 +0900

    format

--- lua/swpclear.lua
+++ lua/swpclear.lua
@@ -14,7 +14,6 @@ M.setup = function(args)
   M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
-
 -- "hello" is a public method for the plugin
 M.swpclr = function()
   module.my_first_function()
--- lua/swpclear/module.lua
+++ lua/swpclear/module.lua
@@ -5,14 +5,12 @@ local api = vim.api
 local buf, win
 
 M.my_first_function = function()
-
-print("nasi")
+  print("nasi")
   buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
-  local git_log_output = api.nvim_call_function("system", { 'git log -n 30 --oneline' })
+  local git_log_output = api.nvim_call_function("system", { "git log -n 30 --oneline" })
   local git_logs = vim.split(git_log_output, "\n")
   api.nvim_buf_set_lines(buf, 0, -1, true, git_logs)
-  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-
+  api.nvim_buf_set_option(buf, "bufhidden", "wipe")
 
   local opts = {
     style = "minimal",
@@ -20,7 +18,7 @@ print("nasi")
     width = 100,
     height = 100,
     row = 0,
-    col = 0
+    col = 0,
   }
   win = api.nvim_open_win(buf, true, opts)
   --
