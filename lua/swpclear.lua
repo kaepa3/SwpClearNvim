2764386a168507514539baa7488b615cfcfe789c
kaepa3 <tak_3_zaki3@yahoo.co.jp>
2022-08-16 00:09:03 +0900

    cyo-jiri awase

--- lua/swpclear.lua
+++ lua/swpclear.lua
@@ -14,11 +14,10 @@ M.setup = function(args)
     M.config = vim.tbl_deep_extend("force", M.config, args or {})
 end
 
+
 -- "hello" is a public method for the plugin
 M.swpclr = function()
-    local val = module.my_first_function()
-    print(val)
-
+    module.my_first_function()
 end
 
 return M
--- lua/swpclear/module.lua
+++ lua/swpclear/module.lua
@@ -1,8 +1,25 @@
 -- module represents a lua module for the plugin
 local M = {}
 
+local api = vim.api
+local buf, win
+
 M.my_first_function = function()
-  return "hello world!"
+
+    buf = api.nvim_create_buf(false, true) -- create new emtpy buffer
+    api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+
+    local opts = {
+        style = "minimal",
+        relative = "editor",
+        width = 100,
+        height = 100,
+        row = 0,
+        col = 0
+    }
+    win = api.nvim_open_win(buf, true, opts)
+    --
+    return "ahello world!"
 end
 
 return M
